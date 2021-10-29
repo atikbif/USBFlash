09b235a50e53acf37cf7a9e4e6c34295de78881a
Roman Pelevin <pelevin.roman@gmail.com>
2021-10-29 12:00:52 +0300

    makefile

---
+++ Release/Core/Src/subdir.mk
@@ -0,0 +1,53 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Core/Src/at45db081e.c \
+../Core/Src/device_state.c \
+../Core/Src/dma.c \
+../Core/Src/gpio.c \
+../Core/Src/led.c \
+../Core/Src/main.c \
+../Core/Src/spi.c \
+../Core/Src/stm32l1xx_hal_msp.c \
+../Core/Src/stm32l1xx_it.c \
+../Core/Src/syscalls.c \
+../Core/Src/sysmem.c \
+../Core/Src/system_stm32l1xx.c 
+
+OBJS += \
+./Core/Src/at45db081e.o \
+./Core/Src/device_state.o \
+./Core/Src/dma.o \
+./Core/Src/gpio.o \
+./Core/Src/led.o \
+./Core/Src/main.o \
+./Core/Src/spi.o \
+./Core/Src/stm32l1xx_hal_msp.o \
+./Core/Src/stm32l1xx_it.o \
+./Core/Src/syscalls.o \
+./Core/Src/sysmem.o \
+./Core/Src/system_stm32l1xx.o 
+
+C_DEPS += \
+./Core/Src/at45db081e.d \
+./Core/Src/device_state.d \
+./Core/Src/dma.d \
+./Core/Src/gpio.d \
+./Core/Src/led.d \
+./Core/Src/main.d \
+./Core/Src/spi.d \
+./Core/Src/stm32l1xx_hal_msp.d \
+./Core/Src/stm32l1xx_it.d \
+./Core/Src/syscalls.d \
+./Core/Src/sysmem.d \
+./Core/Src/system_stm32l1xx.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/Src/%.o: ../Core/Src/%.c Core/Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -DUSE_HAL_DRIVER -DSTM32L151xBA -c -I../Core/Inc -I../Drivers/STM32L1xx_HAL_Driver/Inc -I../Drivers/STM32L1xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32L1xx/Include -I../Drivers/CMSIS/Include -I../USB_DEVICE/App -I../USB_DEVICE/Target -I../Middlewares/ST/STM32_USB_Device_Library/Core/Inc -I../Middlewares/ST/STM32_USB_Device_Library/Class/MSC/Inc -Os -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
---
+++ Release/USB_DEVICE/App/subdir.mk
@@ -0,0 +1,26 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../USB_DEVICE/App/usb_device.c \
+../USB_DEVICE/App/usbd_desc.c \
+../USB_DEVICE/App/usbd_storage_if.c 
+
+OBJS += \
+./USB_DEVICE/App/usb_device.o \
+./USB_DEVICE/App/usbd_desc.o \
+./USB_DEVICE/App/usbd_storage_if.o 
+
+C_DEPS += \
+./USB_DEVICE/App/usb_device.d \
+./USB_DEVICE/App/usbd_desc.d \
+./USB_DEVICE/App/usbd_storage_if.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+USB_DEVICE/App/%.o: ../USB_DEVICE/App/%.c USB_DEVICE/App/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -DUSE_HAL_DRIVER -DSTM32L151xBA -c -I../Core/Inc -I../Drivers/STM32L1xx_HAL_Driver/Inc -I../Drivers/STM32L1xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32L1xx/Include -I../Drivers/CMSIS/Include -I../USB_DEVICE/App -I../USB_DEVICE/Target -I../Middlewares/ST/STM32_USB_Device_Library/Core/Inc -I../Middlewares/ST/STM32_USB_Device_Library/Class/MSC/Inc -Os -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
